257515aea1b9344eeee1cbdc40fbcb862beba5d8
Leonardo Almeida <leonardograselalmeida@gmail.com>
2022-06-12 14:56:51 -0300

    feat(app) add screen to save my notes

--- MinhasAnotacoes/ViewController.swift
+++ MinhasAnotacoes/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let chave = "minhaAnotacao"
+    @IBOutlet weak var textoCampo: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textoCampo.text = recuperarAnotacao()
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func salvarAnotacao(_ sender: Any) {
+        if let texto = textoCampo.text {
+            UserDefaults.standard.set(texto, forKey: chave)
+        }
+        view.endEditing(true)
+    }
+    
+    func recuperarAnotacao() -> String {
+        if let textoRecuperado = UserDefaults.standard.object(forKey: chave) {
+            return textoRecuperado as! String
+        }
+        
+        return ""
+    }
 
 }
 
